067f509cdb202623af0af32b0f1a822f651d34c9
Mriganka Paul <mpaul@redhat.com>
2024-06-07 12:24:39 -0400

    Updated scale-llama script

--- nim-runtime-published/a100/24.05/in-memory/scale-llama.sh
+++ nim-runtime-published/a100/24.05/in-memory/scale-llama.sh
@@ -7,5 +7,5 @@ curl --insecure \
 -k "${1}" \
 -d "@llama.json" \
 -H "Content-Type: application/json" \
--H "Authorization: Bearer $API_KEY_REQUIRED_IF_EXECUTING_OUTSIDE_NGC"  > result-"${i}"&
+-H "Authorization: Bearer $API_KEY_REQUIRED_IF_EXECUTING_OUTSIDE_NGC"  > tmp/result-"${i}"&
 done
